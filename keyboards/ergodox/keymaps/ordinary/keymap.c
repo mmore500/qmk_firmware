386736d9780b36592aeeb630d8cc95de928d44c4
Matthew Andres Moreno <matthew.andres.moreno@gmail.com>
2018-05-18 08:40:39 -0400

    Add pinky escape key

--- keyboards/ergodox/keymaps/ordinary/keymap.c
+++ keyboards/ergodox/keymaps/ordinary/keymap.c
@@ -156,7 +156,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  KC_TRNS ,KC_F11   ,KC_F12  ,KC_F13   ,KC_F14  ,KC_F15  ,KC_ESC
 ,KC_TRNS ,KC_POWER ,M(MUL)  ,KC_MS_U  ,M(MUR)  ,KC_VOLU ,KC_WH_U
 ,KC_TRNS ,KC_SLEP  ,KC_MS_L ,KC_MS_D  ,KC_MS_R ,KC_VOLD
-,KC_TRNS ,KC_NO    ,M(MDL)  ,KC_MS_D  ,M(MDR)  ,KC_MUTE ,KC_WH_D
+,KC_TRNS ,KC_ESC   ,M(MDL)  ,KC_MS_D  ,M(MDR)  ,KC_MUTE ,KC_WH_D
 ,KC_NO ,KC_NO   ,KC_BTN3 ,KC_BTN1  ,KC_BTN2
                                                ,KC_WSTP ,KC_WREF
                                                         ,KC_WSCH
